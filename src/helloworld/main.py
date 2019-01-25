da6203fa43aa7789dc23d73bc4b7c0fe8f6eea7d
floyebolu <zcbeg36@ucl.ac.uk>
2019-01-25 03:18:47 +0000

    Initial Commit
    Lecture 25

---
+++ src/helloworld/main.py
@@ -0,0 +1,11 @@
+
+
+run = True
+current =  1
+
+while run:
+    if current == 100:
+        run = False
+    else:
+        print(current)
+        current += 1
---
+++ src/helloworld/simplemain.py
@@ -0,0 +1,14 @@
+
+
+print("Hello World!")
+
+b = 3**3
+
+
+c = ("You are " + str(b) + " now.")
+
+print(c.__len__())
+
+print(c)
+
+print(len(c))
